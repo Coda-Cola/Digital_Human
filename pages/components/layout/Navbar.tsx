4e14262efd0ae5270ddf976763b924552fdda992
oda <ooddaa@gmail.com>
2022-07-06 17:37:53 +0100

    [deploy] to Vercel - wrapped <a> in Link from next/link

--- pages/components/layout/Navbar.tsx
+++ pages/components/layout/Navbar.tsx
@@ -1,20 +1,22 @@
 import Image from "next/image";
-import { useSession, signIn, signOut } from "next-auth/react"
-
+import { useSession, signIn, signOut } from "next-auth/react";
+import Link from "next/link";
 
 const styles = {
-  signInBtn: "inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-700 hover:bg-white mt-4 md:mt-0",
-  link: "block mt-4 sm:inline-block sm:mt-0 text-teal-300 hover:text-white mr-4"
-}
+  signInBtn:
+    "inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-700 hover:bg-white mt-4 md:mt-0",
+  link: "block mt-4 sm:inline-block sm:mt-0 text-teal-300 hover:text-white mr-4",
+};
 
 function Navbar() {
   const { status } = useSession();
-  const isAuthenticated = (status === 'authenticated')
-  
+  const isAuthenticated = status === "authenticated";
+
   const userLinks = [
-    <a key={"dashboard-link"} href="/dashboard" className={styles.link}>
-      Dashboard
-    </a>,
+    <Link key={"dashboard-link"} href="/dashboard">
+      <a className={styles.link}>Dashboard</a>
+      {}
+    </Link>,
   ];
   return (
     <div className="navbar">
@@ -27,25 +29,27 @@ function Navbar() {
             src="/assets/img/logo.png"
             alt="logo"
           />
-          <a href="/">
-            <span className="font-bold text-xl">Digital Human</span>
-          </a>
+          <Link href="/">
+            <a className={styles.link}>
+              <span className="font-bold text-xl">Digital Human</span>
+            </a>
+          </Link>
         </div>
 
         <div className="w-full block flex-grow sm:flex sm:items-center sm:w-auto">
           <div className="text-sm sm:flex-grow">
-            <a href="#" className={styles.link}>
-              ID data provider
-            </a>
-            <a href="#" className={styles.link}>
-              ID data consumer
-            </a>
-            <a href="#" className={styles.link}>
-              About
-            </a>
-            <a href="#" className={styles.link}>
-              Team
-            </a>
+            <Link href="#">
+              <a className={styles.link}>ID data provider</a>
+            </Link>
+            <Link href="#">
+              <a className={styles.link}>ID data consumer</a>
+            </Link>
+            <Link href="#">
+              <a className={styles.link}>About</a>
+            </Link>
+            <Link href="#">
+              <a className={styles.link}>Team</a>
+            </Link>
             {/* show Dashboard link if user has authenticated */}
             {isAuthenticated && userLinks}
           </div>
@@ -53,7 +57,7 @@ function Navbar() {
             {isAuthenticated ? (
               <button
                 className={styles.signInBtn}
-                onClick={() => signOut({ callbackUrl: '/' })}
+                onClick={() => signOut({ callbackUrl: "/" })}
               >
                 Log out
               </button>
